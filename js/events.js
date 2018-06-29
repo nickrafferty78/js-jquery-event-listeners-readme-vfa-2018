c1e18a58d8da900e27d92ea55a2826b8b5c0f846
Nick Rafferty <nickrafferty78@gmail.com>
2018-06-29 20:40:47 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,35 @@
 //define functions here
+function getIt(){
+$( 'p' ).click(function() {
+  alert( "Hey!" );
+});
+}
+function frameIt(){
+  $("img").load(function(){
+    $('img').addClass("tasty");
+});
+}
+
+function pressIt(){
+    $( "#typing" ).on( "keydown", function( event ) {
+  if (event.which == 71){
+    alert();
+  }
+});
+}
+
+function submitIt(){
+$("form").submit(function(){
+    alert("Your form is going to be submitted now.");
+});
+}
 
 $(document).ready(function(){
 
-// call functions here
+
+getIt();
+frameIt();
+pressIt();
+submitIt();
 
 });
