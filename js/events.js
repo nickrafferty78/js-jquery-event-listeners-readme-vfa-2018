43e67a82ebf9b619a340eb93152019018628d62e
Nick Rafferty <nickrafferty78@gmail.com>
2018-06-29 20:35:25 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -19,7 +19,7 @@ function pressIt(){
 }
 
 function submitIt(){
- $("#typing").submit(function( event ) {
+ $("#typing").submit(function() {
   alert( "Your form is going to be submitted now." );
 });
 }
