3e02b77c1b9e48d7d3a5e6c52ffd04feb60369ac
Nick Rafferty <nickrafferty78@gmail.com>
2018-06-29 20:17:10 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,33 @@
 //define functions here
+function getIt(){
+$( 'p' ).click(function() {
+  alert( "Hey!" );
+});
+}
+function frameIt(){
+  $("img").load(function(){
+    $('img').addClass("tasty");
+});
+}
+
+function pressIt(){
+  $("#typing").keydown(function(){
+    var x = event.which;
+    if(x==71){
+    alert('G');
+    }
+  });
+}
+function submitIt(){
+  $( "#typing" ).submit(function() {
+  alert( "Your form is going to be submitted now." );
+});
+}
 
 $(document).ready(function(){
 
-// call functions here
+
+getIt();
+frameIt();
 
 });
